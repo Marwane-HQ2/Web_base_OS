dc0062dd34daf08f801c529ed2914a1cc3c42a95
M_ <142575527+Marwane-HQ2@users.noreply.github.com>
2025-01-22 23:19:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,21 +9,21 @@ setInterval(updateTime, 1000)
 
 // FAIRE GLISSER LA FENÊTRE
 
-let welcomeWindow = document.getElementById("welcome")
-let notepadWindow = document.getElementById("notepad")
+// let welcomeWindow = document.getElementById("welcome")
+// let notepadWindow = document.getElementById("notepad")
+// let projectsWindow = document.getElementById("project")
+// let vaultWindow = document.getElementById('vault')
+// let browserWindow = document.getElementById("browser")
+
 let notepadCreateNoteWindow = document.getElementById("notepad-create-note")
-let browserWindow = document.getElementById("browser")
-let projectsWindow = document.getElementById("project")
-let vaultWindow = document.getElementById('vault')
+
+
 
 // Make the DIV element draggable:
-// OK
-dragElement(welcomeWindow)
-dragElement(notepadWindow)
-dragElement(notepadCreateNoteWindow)
-dragElement(browserWindow)
-dragElement(projectsWindow)
-dragElement(vaultWindow)
+
+for (bal of document.querySelectorAll(".window")) {
+  dragElement(bal)
+}
 
 // Step 1: Define a function called `dragElement` that makes an HTML element draggable.
 function dragElement(element) {
@@ -83,7 +83,6 @@ function closeWindow (element) {
 }
 
 function openWindow (element) {
-    console.log(element)
     element.style.display = ""
     biggestIndex += 1
     element.style.zIndex = biggestIndex
@@ -129,14 +128,9 @@ function handleWindowTap (element) {
 }
 
 
-// OK
-addWindowTapHandling(welcomeWindow)
-addWindowTapHandling(notepadWindow)
-addWindowTapHandling(notepadCreateNoteWindow)
-addWindowTapHandling(browserWindow)
-addWindowTapHandling(projectsWindow)
-addWindowTapHandling(vaultWindow)
-
+for (bal of document.querySelectorAll(".window")) {
+  addWindowTapHandling(bal)
+}
 
 // NOTEPAD NOTES
 let notes =  [
@@ -264,4 +258,134 @@ function pinCodeCheck () {
       document.getElementById('pin-hint').style.visibility = "visible"
     }
   }
+}
+
+// PIANO.EXE
+Tone.start()
+
+
+const synth = new Tone.Synth().toDestination();
+
+const piano = document.getElementById("piano")
+const table_notes = [261, 277, 311, 329, 349, 369, 415, 440, 466, 493]
+const table_notes_name = ['do', 're', 'mi', 'fa', 'sol', 'la', 'si', 'do_', 're_', 'l']
+const touches_notes = {
+    "Q": 0,
+    "S": 1,
+    "D": 2,
+    "F": 3,
+    "G": 4,
+    "H": 5,
+    "J": 6,
+    "K": 7,
+    "L": 8,
+    "M": 9
+}
+
+play_functions = []
+stop_functions = []
+
+for (let i=0; i < table_notes.length;i++) {
+    let element = document.createElement("div")
+    element.classList = "tile"
+    element.id = `tile${i+1}`
+    element.innerHTML = `Tile ${i+1}`
+    let play_tile = function () {
+        synth.triggerAttack(table_notes[i])
+        element.style.backgroundColor = "#547951"
+    }
+    let stop = function () {
+        synth.triggerRelease()
+        element.style.backgroundColor = "#7cff70ad"
+    }
+
+    play_functions.push(play_tile)
+    stop_functions.push(stop)
+    
+    element.addEventListener("pointerdown", play_tile)
+    element.addEventListener("pointerup", stop)
+    
+
+    piano.appendChild(element)
+}
+
+let key_down = function (event) {
+    e = event.key.toUpperCase()
+    if (!touches_notes[e] && !(touches_notes[e] === 0)) {return}
+    if (e) {
+        synth.triggerAttack(table_notes[touches_notes[e]])
+        element = document.getElementById(`tile${touches_notes[e]+1}`)
+        element.style.backgroundColor = "#547951"
+    }
+
+}
+
+let key_up = function (event) {
+    synth.triggerRelease()
+    e = event.key.toUpperCase()
+    if (touches_notes[e] || touches_notes[e] === 0) {
+        element = document.getElementById(`tile${touches_notes[e]+1}`)
+        element.style.backgroundColor = "#7cff70ad"
+    }
+    
+}
+let idKeyDown
+let idKeyUp
+
+function addKeysEventListener () {
+  idKeyDown = document.body.addEventListener("keydown", key_down)
+  idKeyUp = document.body.addEventListener("keyup", key_up)
+}
+
+function removeKeysEventListener () {
+  document.body.removeEventListener("keydown", key_down)
+  document.body.removeEventListener("keyup", key_up)
+}
+
+// MODIFIER LA NOTE 
+let list_tiles = piano.children
+
+function change_note() {
+    for (let i=0; i<list_tiles.length; i++) {
+        child = list_tiles[i]
+        child.removeEventListener("pointerdown", play_functions[i])
+        child.removeEventListener("pointerup", stop_functions[i])
+        
+        let play_tile = function () {
+            synth.triggerAttack(table_notes[i])
+            list_tiles[i].style.backgroundColor = "#547951"
+        }
+        let stop = function () {
+            synth.triggerRelease()
+            list_tiles[i].style.backgroundColor = "#7cff70ad"
+        }
+
+        child.addEventListener("pointerdown", play_tile)
+        child.addEventListener("pointerup", stop)
+
+        play_functions[i] = play_tile
+        stop_functions[i] = stop
+    }
+    document.body.removeEventListener("keydown", key_down)
+    document.body.removeEventListener("keyup", key_up)
+
+    key_down = function (event) {
+        e = event.key.toUpperCase()
+        if (touches_notes[e] || touches_notes[e] == 0) {
+            synth.triggerAttack(table_notes[touches_notes[e]])
+            element = document.getElementById(`tile${touches_notes[e]+1}`)
+            element.style.backgroundColor = "#547951"
+        }
+    }
+
+    key_up = function (event) {
+        synth.triggerRelease()
+        e = event.key.toUpperCase()
+        if (touches_notes[e] || touches_notes[e] == 0) {
+            element = document.getElementById(`tile${touches_notes[e]+1}`)
+            element.style.backgroundColor = "#7cff70ad"
+        }
+    }
+    document.body.addEventListener("keydown", key_down)
+    document.body.addEventListener("keyup", key_up)
 }
